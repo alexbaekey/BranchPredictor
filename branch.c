60b5dc01e60448114cf57c3fe4b13ac95fadf3db
abaekey1 <abaekey@knights.ucf.edu>
2020-11-06 15:22:22 -0500

    non-working smith and bimodal predictors (: no code help during office hours lol

--- branch.c
+++ branch.c
@@ -17,9 +17,14 @@ int mispredictions=0;
 int predictions=0;
 int B, M2, M1, N, K;
 
+int count;
+
+int *bimodPT;
 //Evaluation Metrics
 int CPI;
 int speedup;
+
+int bimodsize;
 
 // hazards?
      // data
@@ -29,7 +34,7 @@ int speedup;
 //functions
 void smith(int branchPC, char outcome);
 void predict(int branchPC, char outcome);
-
+void bimodal(int branchPC, char outcome, int M2, int bisize);
 
 
 
@@ -71,10 +76,7 @@ void smith(int branchPC, char outcome){
                     countArr[cur] = countArr[cur] - 1;
                }
           }
-
-
      }
-
      if(countArr[cur]>=mid){
           //predicted taken
           if(outcome == 'n'){
@@ -91,24 +93,104 @@ void smith(int branchPC, char outcome){
                }
           }
      }
-
 }
 
 
 
+void smith2(int branchPC, char outcome){
+
+/*     for(int j=0;j<1024;j++){*/
+/*          printf("countArr %d, %d\n", j, countArr[j]);*/
+/*     }*/
+     predictions++;
+     printf("branchPC: %d\n", branchPC);
+     printf("outcome: %c\n", outcome);
+/*     printf("value of cur: %d\n", cur);*/
+/*     printf("count array: %d\n", countArr[0]);*/
+/*     printf("counternum %d\n", countArr[cur]);*/
+     int mid = (int)(pow(2,(float)B)/2.);
+     int max = (int)(pow(2,(float)B));
+     //TODO CHECK THESE CONDITIONS
+     if(count<mid){
+          //predicted not taken
+          if(outcome == 't'){
+               mispredictions++;
+               //updatecounter
+               if(count<(max-1)){
+                    count = count + 1;
+               }
+          }
+          if(outcome == 'n'){
+               if(count>0){
+                    count = count - 1;
+               }
+          }
+     }
+     if(count>=mid){
+          //predicted taken
+          if(outcome == 'n'){
+               mispredictions++;
+               //updatecounter
+               if(count>0){
+                    count = count - 1;
+               }
+          }
+          if(outcome == 't'){
+               //updatecounter
+               if(count<(max-1)){
+                    count = count + 1;
+               }
+          }
+     }
+}
+
+
+void bimodal(int branchPC, char outcome, int M2, int bisize){
+
+     predictions++;
+     //use bits M+1 to 2 of PC
+     int cur = (((1<<(M2))-1) & (branchPC>>(2)));
+     int mid = 4;
+     int max = 8;
+     printf("cur: %d\n", cur);
+     if(bimodPT[cur]<mid){
+          //predicted not taken
+          if(outcome == 't'){
+               mispredictions++;
+               //updatecounter
+               if(bimodPT[cur]<(max-1)){
+                    bimodPT[cur] = bimodPT[cur] + 1;
+               }
+          }
+          if(outcome == 'n'){
+               if(bimodPT[cur]>0){
+                    bimodPT[cur] = bimodPT[cur] - 1;
+               }
+          }
+     }
+     if(bimodPT[cur]>=mid){
+          //predicted taken
+          if(outcome == 'n'){
+               mispredictions++;
+               //updatecounter
+               if(bimodPT[cur]>0){
+                    bimodPT[cur] = bimodPT[cur] - 1;
+               }
+          }
+          if(outcome == 't'){
+               //updatecounter
+               if(bimodPT[cur]<(max-1)){
+                    bimodPT[cur] = bimodPT[cur] + 1;
+               }
+          }
+     }
+}
 
 
 // main execution
 
 void main(int argc, char *argv[]){
 
-     //   User Input Attributes //
-
-/*     printf("%d\n", argc);*/
-/*     printf("%s\n", argv[0]);     */
-/*     printf("%s\n", argv[1]);     */
-
-     //   Read in trace file  //
      char outcome;
      int branchPC;
      FILE* fp;
@@ -116,37 +198,40 @@ void main(int argc, char *argv[]){
      char *trace;
      char *predictor;
 
-     
-
      //read in type of branch predictor, loop through file
      predictor = argv[1];
-//while ( != 0)
      if(strcmp("smith", predictor) == 0){
           B = strtol(argv[2], NULL, 10);
           trace = argv[3];
           //initialize counter array of size 2^10
           //TODO JUST CAUSE IDK WHAT SIZE TO MAKE THIS ARRAY
-          //
-          countArr = (int *)malloc(sizeof(int)*1024);
+          //countArr = (int *)malloc(sizeof(int)*1024);
           int x = (int)(pow(2,(float)B)/2.);
-          for(int i=0; i<1024; i++){
-               //NOT SURE IF RIGHT START VALUE  ROUND DOWN? //TODO
-               countArr[i] = x;
-          }
+          count = x;
+/*          old way assuming BHT (indexed table)*/
+/*          for(int i=0; i<1024; i++){*/
+/*               countArr[i] = x;*/
+/*          }*/
           fp = fopen(trace, "r");
           while(!feof(fp)){   
                fscanf(fp, "%x\n %c", &branchPC, &outcome);
-               smith(branchPC, outcome); 
+               smith2(branchPC, outcome); 
           }  
           fclose(fp);
      }
      else if(strcmp("bimodal", predictor) == 0){
           M2 = strtol(argv[2], NULL, 10);
           trace = argv[3];
+          int bisize = (int)(pow(2,(float)M2));
+          bimodsize = bisize;
+          bimodPT = (int *)malloc(sizeof(int)*bisize);
+          for(int i=0; i<bisize; i++){
+               bimodPT[i] = 4;
+          }
           fp = fopen(trace, "r");
           while(!feof(fp)){   
                fscanf(fp, "%x\n %c", &branchPC, &outcome);
-               predict(branchPC, outcome);   
+               bimodal(branchPC, outcome, M2, bisize);   
           }  
           fclose(fp);
      }
@@ -182,6 +267,11 @@ void main(int argc, char *argv[]){
      printf("Number of predictions: %d\n", predictions);
      printf("Number of mispredictions: %d\n", mispredictions);
 
+     if(strcmp("bimodal", predictor) == 0){
+          for(int i=0;i<bimodsize;i++){
+               printf("%d %d\n",i, bimodPT[i]);
+          }
+     }
 
 /*//not sure if necessary*/
 /*     //firstline?*/
